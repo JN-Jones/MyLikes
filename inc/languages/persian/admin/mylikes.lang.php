7ada71fd3e1f15c5f1ee306131a2807fbd3e33d7
JN-Jones <Jones_h1@web.de>
2014-11-06 14:23:39 +0100

    Adding persian language files

---
+++ inc/languages/persian/admin/mylikes.lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$l['mylikes_error_enablereputation'] = "شما باید <a href=\"index.php?module=config-settings&action=change&gid={1}\">سیستم شهرت را فعال کنید</a>";
+$l['mylikes_error_postrep'] = "شما باید <a href=\"index.php?module=config-settings&action=change&gid={1}\">پست شهرت را فعال کنید</a>";
+$l['mylikes_error_posrep'] = "You need toشما باید <a href=\"index.php?module=config-settings&action=change&gid={1}\">خواننده مثبت را فعال کنید</a>";
---
+++ inc/languages/persian/mylikes.lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$l['mylikes_like'] = "پسندیدن";
+$l['mylikes_unlike'] = "نپسندیدن";
+$l['mylikes_likes'] = "پسند (ها)";
+$l['mylikes_no_likes'] = "این پست توسط کسی پسندیده نشد :(";
