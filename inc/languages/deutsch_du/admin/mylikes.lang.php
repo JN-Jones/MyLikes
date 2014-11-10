8668800b6eb21d1aa7baf337488a7c95f47fcf2c
JN-Jones <jones_h1@web.de>
2014-11-10 14:05:40 +0100

    German language package

---
+++ inc/languages/deutsch_du/admin/mylikes.lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$l['mylikes_error_enablereputation'] = "Du musst das <a href=\"index.php?module=config-settings&action=change&gid={1}\">Bewertungssystem aktivieren.</a>";
+$l['mylikes_error_postrep'] = "Du musst die <a href=\"index.php?module=config-settings&action=change&gid={1}\">Beitrags-Bewertungssystem aktivieren.</a>";
+$l['mylikes_error_posrep'] = "Du musst <a href=\"index.php?module=config-settings&action=change&gid={1}\">positive Bewertungen aktivieren.</a>";
---
+++ inc/languages/deutsch_du/mylikes.lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$l['mylikes_like'] = "Like +";
+$l['mylikes_unlike'] = "Like -";
+$l['mylikes_likes'] = "Like(s)";
+$l['mylikes_no_likes'] = "Dieser Beitrag gefällt bisher noch niemandem.";
