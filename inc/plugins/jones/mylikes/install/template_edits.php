7013da659186a734bf4ca449fea7fd0a4f98097a
JN-Jones <Jones_h1@web.de>
2015-06-05 12:24:45 +0200

    Finish MyLikes, fix the installer and use the stable version of JonesCore

--- inc/plugins/jones/mylikes/install/template_edits.php
+++ inc/plugins/jones/mylikes/install/template_edits.php
@@ -8,9 +8,7 @@ $edits = array(
 		'{$post[\'button_rep\']}' => '{$post[\'button_like\']}',
 	),
 	"headerinclude"	=> array(
-		'// -->
-</script>' => '// -->
-</script>
+		'{$stylesheets}' => '{$stylesheets}
 <script type="text/javascript" src="{$mybb->asset_url}/jscripts/mylikes.js?ver=101"></script>',
 	),
 );
--- inc/plugins/jones/mylikes/version/V101.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class JB_MyLikes_Version_V101 extends JB_Version_Base
-{
-	static function execute()
-	{
-		
-	}
-}
